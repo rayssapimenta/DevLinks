386ab01c84f81fa351422e1bbda514fb52e0ec0f
rayssapimenta <ralpimenta@hotmail.com>
2025-08-17 02:31:17 -0300

    ajustandp js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function toggleModel(){
     const html = document.documentElement
+    
+    html.classList.toggle('light')
 
-//   if(html.classList.contains('light')){
- //   html.classList.remove('light')
-//   }else{
-  //  html.classList.add('light')
- //  }
-html.classList.toggle('light')
+    const img = document.querySelector("#profile img")
+
+if(html.classList.contains('light')){
+    img.setAttribute('src','https:\\github.com/rayssapimenta.png')
+    //document.documentElement.querySelector('alt')
+}else{
+    img.setAttribute('src','./assets/dark.png')
+
+}
 }
